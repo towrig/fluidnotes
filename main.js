e92620f792ebb9abcca2dcadba6886d21815f123
towrig <karl32123@gmail.com>
2024-06-30 10:34:54 +0300

    add cannon-es

--- main.js
+++ main.js
@@ -1,26 +1,60 @@
 import * as THREE from 'three';
+import * as CANNON from 'cannon-es'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
+document.body.appendChild(renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
 
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize(window.innerWidth, window.innerHeight);
 
+/*
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
-
+*/
 camera.position.z = 5;
 
-document.body.appendChild( renderer.domElement );
+const world = new CANNON.World({
+    gravity: new CANNON.Vec3(0, -9.82, 0), // m/s²
+})
 
+//Sphere
+const radius = 1; // m
+const sphereBody = new CANNON.Body({
+    mass: 2, // kg
+    angularDamping: 1,
+    shape: new CANNON.Sphere(radius),
+})
+sphereBody.position.set(0, 2, 0) // m
+world.addBody(sphereBody)
+const geometry = new THREE.SphereGeometry(radius)
+const material = new THREE.MeshNormalMaterial()
+const sphereMesh = new THREE.Mesh(geometry, material)
+scene.add(sphereMesh)
+
+//Ground
+const groundBody = new CANNON.Body({
+    type: CANNON.Body.STATIC,
+    shape: new CANNON.Plane(),
+    position: new CANNON.Vec3(0, -2, 0),
+})
+groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0) // make it face up
+world.addBody(groundBody)
+
+
+// main loop
 function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-	renderer.render( scene, camera );
+    //do physics
+    //sphereBody.applyForce()
+    world.fixedStep();
+    sphereMesh.position.copy(sphereBody.position)
+    sphereMesh.quaternion.copy(sphereBody.quaternion)
+    
+    //render
+    renderer.render(scene, camera);
 }
-renderer.setAnimationLoop( animate );
-
-const controls = new OrbitControls( camera, renderer.domElement );
+renderer.setAnimationLoop(animate);
